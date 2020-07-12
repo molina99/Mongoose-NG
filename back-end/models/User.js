6b884484d2c472a77352b10a120e297242c7ed53
molina99 <darkmolina99@gmail.com>
2020-07-11 23:33:42 -0500

    encriptacion password, autenticacion

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -7,7 +7,6 @@ const {Schema} = mongoose
 const UserSchema = new Schema({
     names: {type: String, required: true},
     lastNames: {type: String, required: true},
-    // photo: {type: String},
     email: {type: String, required: true},
     password: {type: String, required: true}
 })
